2578033b12d77d35ce937919f892e38dfeb43967
KleaRJ <69857959+KleaRJ@users.noreply.github.com>
2021-05-16 13:46:37 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Do from './methodstorendervideo/do';
+import Modal from './methodstorendervideo/modal';
+
+export default function App() {
+  return (
+    <Modal />
+  );
+}
